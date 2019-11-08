7b5f124be67871a42b945e7a85e1954f6cd0c403
oleg-gorodnitchi <oleggorj@hotmail.com>
2019-11-08 17:59:15 -0500

    minor fixes, breaking down into modules

--- setup-managed-identity.sh
+++ setup-managed-identity.sh
@@ -1,46 +1,73 @@
 #!/usr/bin/env bash
-
-app_name="app1"
-vaultRG="kv-test"
-clusterRG="DefaultResourceGroup-CUS"
-cluster="operator-test-cluster"
-namespace="${app_name}-d-ns"
-principal="${app_name}-principal"
-binding="${app_name}-principal-binding"
-podSelector="pod-selector-label"
-registryname="securityopregistrytest"
-reponame="env_injector"
-azAccount="Learning - Oleg Gorodnitchi"
+source ./vars.sh
 
 az account set -s "$azAccount"
 
-az aks get-credentials --name $cluster --resource-group $clusterRG
+az aks get-credentials --name $clusterName --resource-group $clusterRG
+
+subscriptionId=$(az account show --query "id")
+clusterRGScope=$(az group show --name  $clusterRG --query "id")
+clusterRGScope=$(sed -e 's/^"//' -e 's/"$//' <<<$clusterRGScope)
 
 # super important
 kubectl apply -f https://raw.githubusercontent.com/Azure/aad-pod-identity/master/deploy/infra/deployment-rbac.yaml
 
+# Identity
 echo "Creating Identity.."
-identityJson=$(az identity create -g $clusterRG -n $principal --query "{ClientId: clientId, ManagedIdentityId: id, TenantId:  tenantId}" -o jsonc)
+identJson=$(az identity create -g $clusterRG -n $principal -o json)
+identityJson=$(az identity show -g $clusterRG -n $principal -o json)
+identityPrincipalId=$(echo $identityJson | jq -r '.principalId')
+identityScope=$(echo $identityJson | jq -r '.id') && echo "Identity Scope: $identityScope"
+clientId=$(echo $identityJson | jq -r '.clientId') && echo "Identity ClientId: $clientId"
 
-identityJson=$(az identity show -g $clusterRG -n $principal)
-tenantId=$(echo $identityJson | jq '.tenantId') && echo "Identity TenantId: $tenantId"
-clientId=$(echo $identityJson | jq '.clientId') && echo "Identity ClientId: $clientId"
-scope=$(echo $identityJson | jq '.id') && echo "Identity Scope: $scope"
+sleep 15
 
-aksPrincipalId=$(az aks show -g $clusterRG -n $cluster --query "servicePrincipalProfile.clientId" -o tsv) && echo "AKS PrincipalId: $aksPrincipalId"
-managedId=$(az identity show -g $clusterRG -n $principal --query "id" -o tsv) && echo "AKS Identity ID: $managedId"
-assignmentJSON=$(az role assignment create --role "Managed Identity Operator" --assignee $aksPrincipalId --scope $managedId)
-echo "assignmentJSON: $assignmentJSON"
-#az role assignment list --scope $managedId
+# assign identity to Cluster RG
+roleAssignJson=$(az role assignment create --role Reader --assignee $identityPrincipalId --scope $clusterRGScope)
+# identityJson=$(az identity create -g $clusterRG -n $principal --query "{ClientId: clientId, ManagedIdentityId: id, TenantId:  tenantId}" -o jsonc)
 
-# registry
-registryid=$(az acr show --name $registryname --query id --output tsv)
-assignmentJson=$(az role assignment create --assignee $aksPrincipalId --scope $registryid --role acrpull)
+# Vault vars
+keyvaultRGScope=$(sed -e 's/^"//' -e 's/"$//' <<<$(az group show --name  $keyvaultRG --query "id"))
+# assign identity to Vault RG
+roleAssignJson=$(az role assignment create --role Reader --assignee $identityPrincipalId --scope $keyvaultRGScope)
+# set policy to access vault
+roleAssignJson=$(az keyvault set-policy --name $keyvaultName --object-id $identityPrincipalId --secret-permissions get list)
 
+
+# ACR vars
+registryid=$(sed -e 's/^"//' -e 's/"$//' <<<$(az acr show --name $registryname --query id --output tsv))
+# assign identity to ACR
+roleAssignJson=$(az role assignment create --assignee $identityPrincipalId --scope $registryid --role acrpull)
+
+# AKS vars
+aksPrincipalId=$(sed -e 's/^"//' -e 's/"$//' <<<$(az aks show -g $clusterRG -n $clusterName --query  "servicePrincipalProfile.clientId" -o tsv)) && echo "AKS Service Principal: $aksPrincipalId"
+# assign AKS service principal to identity
+roleAssignJson=$(az role assignment create --role "Managed Identity Operator" --assignee $aksPrincipalId --scope $identityScope)
+roleAssignJson=$(az role assignment create --assignee $aksPrincipalId --scope $registryid --role acrpull)
+
+#identityJson=$(az identity show -g $clusterRG -n $principal)
+#tenantId=$(echo $identityJson | jq '.tenantId') && echo "Identity TenantId: $tenantId"
+#clientId=$(echo $identityJson | jq '.clientId') && echo "Identity ClientId: $clientId"
+#principalId=$(echo $identityJson | jq '.principalId')  && echo "Identity PrincipalId: $principalId"
+#scope=$(echo $identityJson | jq '.id') && echo "Identity Scope: $scope"
+#managedId=$(az identity show -g $clusterRG -n $principal --query "id" -o tsv) && echo "Identity ID: $managedId"
+
+# Link Identity to AKS
+#assignmentJSON=$(az role assignment create --role "Managed Identity Operator" --assignee $aksPrincipalId --scope $managedId)
+#echo "assignmentJSON: $assignmentJSON"
+
+# vault
+#az role assignment create --role Reader --assignee <principalid> --scope /subscriptions/<subscriptionid>/resourcegroups/<resourcegroup>/providers/Microsoft.KeyVault/vaults/<keyvaultname>
+# set policy to access keys in your Key Vault
+#az keyvault set-policy -n $KV_NAME --key-permissions get --spn <YOUR AZURE USER IDENTITY CLIENT ID>
+# set policy to access secrets in your Key Vault
+#az keyvault set-policy -n $KV_NAME --secret-permissions get --spn <YOUR AZURE USER IDENTITY CLIENT ID>
+# set policy to access certs in your Key Vault
+#az keyvault set-policy -n $KV_NAME --certificate-permissions get --spn <YOUR AZURE USER IDENTITY CLIENT ID>
 
 sed -e "
 s|{{ .AppPrincipalName }}|${principal}|g
-s|{{ .ResourceID }}|${scope}|g
+s|{{ .ResourceID }}|${identityScope}|g
 s|{{ .ClientID }}|${clientId}|g
 " identity.template.yaml > identity.yaml
 
@@ -50,18 +77,17 @@ s|{{ .AppPrincipalName }}|${principal}|g
 s|{{ .PodSelectorLabel }}|${podSelector}|g
 " binding.template.yaml > binding.yaml
 
-
 kubectl create namespace $namespace
 kubectl label namespace/$namespace description=PodIdentity PodIdentity=true
 
 kubectl apply -f identity.yaml --namespace $namespace
 kubectl apply -f binding.yaml --namespace $namespace
-kubectl get AzureIdentityBinding --namespace $namespace
+kubectl get AzureIdentityBinding --all-namespaces
 
-appId=$(az ad app create --display-name $app_name --identifier-uris http://app1.aad-pod-identity --query "appId" -o tsv)
+appId=$(az ad app create --display-name $app_name --identifier-uris http://${app_name}.aad-pod-identity --query "appId" -o tsv)
 echo $appId
 # OR
-appId=$(az ad app list --display-name $app_name | jq '.[].appId')
+appId=$(az ad app list --display-name $app_name | jq -r '.[].appId')
 az ad app list --app-id $appId
 
 sed -e "
