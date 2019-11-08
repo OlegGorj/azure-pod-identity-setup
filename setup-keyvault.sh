0a4d6aaf718260926fa4dd11342a5fc2897f56dd
oleg-gorodnitchi <oleggorj@hotmail.com>
2019-11-08 17:16:03 -0500

    break down into modules

--- setup-keyvault.sh
+++ setup-keyvault.sh
@@ -1,34 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
 
-appName="application1"
-appCode="AC0001"
-azAccount="..."
-clusterRG="DefaultResourceGroup-CUS"
-clusterName="operator-test-cluster"
+source ./vars.sh
 
-#podSelector="pod-selector-label"
-registryName="securityopregistrytest"
-registryRG=$clusterRG
-#reponame="env_injector"
-
-keyvaultName="az-keyvault-${appName}"
-keyvaultTag="az-keyvault-tag-${appCode}"
-keyvaultRG=$clusterRG
-
-namespace="${appName}-d-ns"
-principal="${appName}-principal"
-binding="${appName}-principal-binding"
 az account set -s "$azAccount"
 
 # create tag for $appName
 #az tag create --name $appCode  --subscription $azAccount
 #az tag add-value --name $appCode --value $appName --subscription $azAccount
 
-kvJson=$(az keyvault create --name $keyvaultName --resource-group $keyvaultRG) && echo "KeyVault created:" && echo $kvJson | jq '.id'
+kvJson=$(az keyvault create --name $keyvaultName --resource-group $keyvaultRG) && echo "KeyVault created:" && echo $kvJson | jq -r '.id'
+
+secretJson=$(az keyvault secret set --vault-name $keyvaultName --name secret1 --value "Don't tell anyone this is secret 1") && echo "Creating secret: id:" && echo $secretJson | jq '.id'
+
 tagsJson=$(az resource tag --tags appcode=$appCode appname=$appName --resource-group $keyvaultRG --name $keyvaultName --resource-type "Microsoft.KeyVault/vaults") && echo "Tagging: provisioningState:" && echo $tagsJson | jq '.properties.provisioningState'
-
-
-##
-# Cleanup
-##
-az keyvault delete --name $keyvaultName  --resource-group $keyvaultRG
